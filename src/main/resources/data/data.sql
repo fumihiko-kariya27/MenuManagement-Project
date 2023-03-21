7546b422aebdd33dc15b70de6f08c1fe3b1268be
kariya-fumihiko27 <fumihiko_kariya@icloud.com>
2023-03-21 22:55:57 +0900

    MyBatis設定の追加

--- src/main/resources/data/data.sql
+++ src/main/resources/data/data.sql
@@ -1,1 +1,4 @@
+/* メニューマスタ　初期データ投入 */
 INSERT INTO cuisine_master(cuisine_name) VALUES('カレーライス'),('パエリア'),('アクアパッツァ'),('ドリア'),('豚肉の生姜焼き'),('ブリの照り焼き');
+/* ユーザーマスタ　初期データ投入 */
+INSERT INTO user_master(user_name, password, role) VALUES('user', 'user_password', 'general'),('system', 'system_password', 'admin');
--- src/main/resources/data/schema.sql
+++ src/main/resources/data/schema.sql
@@ -1,3 +1,5 @@
 DROP TABLE IF EXISTS cuisine_master;
-
+/* メニューマスタ　作成 */
 CREATE TABLE IF NOT EXISTS cuisine_master(cuisine_id INTEGER AUTO_INCREMENT PRIMARY KEY, cuisine_name VARCHAR(100) NOT NULL);
+/* ユーザーマスタ　作成 */
+CREATE TABLE IF NOT EXISTS user_master(user_id INTEGER AUTO_INCREMENT PRIMARY KEY, user_name VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, role VARCHAR(50) NOT NULL);
